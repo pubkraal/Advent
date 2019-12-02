cb481a22ddfaff48afb84b9a65bb49877bc644ed
John Kraal <git@johnkraal.com>
2019-12-02 10:28:59 +0100

    day 1 2019 done

---
+++ 2019/01/01.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import sys
+
+
+def fuel_need(num):
+    return max(0, int(num / 3) - 2)
+
+
+def reduced_fuel_need(num):
+    step = num
+    total = 0
+    for x in range(100):
+        more_fuel = fuel_need(step)
+        if more_fuel == 0:
+            break
+
+        total += more_fuel
+        step = more_fuel
+
+    return total
+
+
+def main(input_file):
+    assert fuel_need(12) == 2
+    assert fuel_need(14) == 2
+    assert fuel_need(1969) == 654
+    assert fuel_need(100756) == 33583
+
+    sum_ = 0
+
+    all_fuels = []
+
+    with open(input_file, 'r') as rd:
+        for line in rd.readlines():
+            num = int(line)
+            fuel = fuel_need(num)
+            all_fuels.append(fuel)
+            sum_ += fuel
+
+    print("Fuel need:", sum_)
+
+    additional_fuels = [reduced_fuel_need(x) for x in all_fuels]
+
+    print("Fuel need w/ fuel:", sum_ + sum(additional_fuels))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
