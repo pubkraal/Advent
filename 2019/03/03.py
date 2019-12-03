3e37ea4efd9b483bc61a8ef89c856ed629f021b0
John Kraal <git@johnkraal.com>
2019-12-03 08:07:49 +0100

    how dare you forget about testcases

--- 2019/03/03.py
+++ 2019/03/03.py
@@ -56,6 +56,8 @@ def main(inputfile):
         assert find_distance(gen_maps(["R8,U5,L5,D3", "U7,R6,D4,L4"])[2]) == 6
         assert find_distance(gen_maps(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"])[2]) == 159
         assert find_distance(gen_maps(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"])[2]) == 135
+        assert find_earliest(*gen_maps(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"])) == 610
+        assert find_earliest(*gen_maps(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"])) == 410
 
     with open(inputfile) as rd:
         paths =[x.strip() for x in rd.readlines()]
