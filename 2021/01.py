a704be16a7b6405018b5679e49a995741e1476da
John Kraal <johnkraal@icloud.com>
2021-12-01 06:53:56 +0100

    2021:01 python cleanup

--- 2021/01.py
+++ 2021/01.py
@@ -5,8 +5,8 @@ from util.input import load_data
 
 
 def main():
-    n = [int(x.strip()) for x in load_data(file_to_day(__file__))]
-    sums = [sum(n[i : i + 3]) for i in range(0, len(n) - 2)]
+    n = list(map(int, load_data(file_to_day(__file__))))
+    sums = [sum(x) for x in zip(n, n[1:], n[2:])]
 
     print("2021:01:1 =", get_incs(n))
     print("2021:01:2 =", get_incs(sums))
@@ -14,13 +14,9 @@ def main():
 
 def get_incs(iter):
     numinc = 0
-    prev = None
+    prev = float("inf")
 
     for cur in iter:
-        if prev is None:
-            prev = cur
-            continue
-
         numinc += cur > prev
         prev = cur
 
