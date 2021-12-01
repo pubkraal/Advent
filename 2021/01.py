a4fd1439ca44dc26f8bce3220134d14c6378511b
John Kraal <johnkraal@icloud.com>
2021-12-01 06:35:26 +0100

    2021:01 python

--- 2021/01.py
+++ 2021/01.py
@@ -5,8 +5,26 @@ from util.input import load_data
 
 
 def main():
-    data = load_data(file_to_day(__file__))
-    print(data)
+    n = [int(x.strip()) for x in load_data(file_to_day(__file__))]
+    sums = [sum(n[i : i + 3]) for i in range(0, len(n) - 2)]
+
+    print("2021:01:1 =", get_incs(n))
+    print("2021:01:2 =", get_incs(sums))
+
+
+def get_incs(iter):
+    numinc = 0
+    prev = None
+
+    for cur in iter:
+        if prev is None:
+            prev = cur
+            continue
+
+        numinc += cur > prev
+        prev = cur
+
+    return numinc
 
 
 if __name__ == "__main__":
