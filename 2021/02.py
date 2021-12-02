b036775045f2f9694faf5823b481e1d7e59b5a19
John Kraal <johnkraal@icloud.com>
2021-12-02 06:56:46 +0100

    2021:02 python

--- 2021/02.py
+++ 2021/02.py
@@ -5,8 +5,29 @@ from util.input import load_data
 
 
 def main():
-    data = load_data(file_to_day(__file__))
-    print(data)
+    aim = 0
+    x, y = 0, 0
+    x2, y2 = 0, 0
+
+    for line in load_data(file_to_day(__file__)):
+        dir, num = line.split()
+        num = int(num)
+        if dir == "forward":
+            x += num
+            x2 += num
+            y2 += aim * num
+        elif dir == "back":
+            x -= num
+            x2 -= num
+        if dir == "down":
+            y += num
+            aim += num
+        elif dir == "up":
+            y -= num
+            aim -= num
+
+    print("2021:02:1 =", x * y)
+    print("2021:02:2 =", x2 * y2)
 
 
 if __name__ == "__main__":
