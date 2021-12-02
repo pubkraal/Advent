518bd399b570b97031e6de8690e528d37b7ffe8e
John Kraal <johnkraal@icloud.com>
2021-12-02 07:21:55 +0100

    2021:02 i was set on using a dict

--- 2021/02.py
+++ 2021/02.py
@@ -6,28 +6,25 @@ from util.input import load_data
 
 def main():
     aim = 0
-    x, y = 0, 0
-    x2, y2 = 0, 0
+    x, y, y2 = 0, 0, 0
 
     for line in load_data(file_to_day(__file__)):
         dir, num = line.split()
         num = int(num)
-        if dir == "forward":
-            x += num
-            x2 += num
-            y2 += aim * num
-        elif dir == "back":
-            x -= num
-            x2 -= num
-        if dir == "down":
-            y += num
-            aim += num
-        elif dir == "up":
-            y -= num
-            aim -= num
+        mx, my, ma, mv = {
+            "forward": (1, 0, 0, 1),
+            "back": (-1, 0, 0, 0),
+            "down": (0, 1, 1, 0),
+            "up": (0, -1, -1, 0),
+        }[dir]
+
+        x += num * mx
+        y += num * my
+        aim += num * ma
+        y2 += (num * mv) * aim
 
     print("2021:02:1 =", x * y)
-    print("2021:02:2 =", x2 * y2)
+    print("2021:02:2 =", x * y2)
 
 
 if __name__ == "__main__":
