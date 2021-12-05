da99eba81577b6a42f62a648a7fdf9f8cffc25db
John Kraal <johnkraal@icloud.com>
2021-12-05 20:48:00 +0100

    added testing to standard layout

--- 2021/10.py
+++ 2021/10.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import sys
+
 from util.aoc import file_to_day
 from util.input import load_data
 
 
-def main():
-    data = load_data(file_to_day(__file__))
+def main(test=False):
+    data = load_data(file_to_day(__file__), test)
     print(data)
 
 
 if __name__ == "__main__":
-    main()
+    test = len(sys.argv) > 1 and sys.argv[1] == "test"
+    main(test)
