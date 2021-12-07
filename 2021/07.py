3d59a2f119586bf0e61db2eda81954ef2908a7e8
John Kraal <johnkraal@icloud.com>
2021-12-07 06:47:32 +0100

    2021:07 python better logic

--- 2021/07.py
+++ 2021/07.py
@@ -2,6 +2,8 @@
 
 import sys
 from functools import cache
+from statistics import median, mean
+from math import floor
 
 from util.aoc import file_to_day
 from util.input import load_data
@@ -12,22 +14,18 @@ def main(test=False):
         map(int, load_data(file_to_day(__file__), test)[0].strip().split(","))
     )
 
-    minfuel = float("inf")
-    minfuel2 = float("inf")
-    for x in range(min(positions), max(positions)):
-        steps = [abs(p - x) for p in positions]
-        distance = sum(steps)
-        distance2 = sum([fuel_cost(x) for x in steps])
-        minfuel = min(distance, minfuel)
-        minfuel2 = min(distance2, minfuel2)
-
-    print("2021:07:1 =", minfuel)
-    print("2021:07:2 =", minfuel2)
+    m = int(median(positions))
+    print("2021:07:1 =", sum([abs(x - m) for x in positions]))
+    m2 = int(round(mean(positions)))
+    p2_min = float("inf")
+    for t in range(m2 - 1, m2 + 2):
+        p2_min = min(sum([fuel_cost(abs(x - t)) for x in positions]), p2_min)
+    print("2021:07:2 =", p2_min)
 
 
 @cache
 def fuel_cost(distance):
-    return sum(list(range(1, distance + 1)))
+    return distance * (distance + 1) // 2
 
 
 if __name__ == "__main__":
