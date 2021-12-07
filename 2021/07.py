4d2fea3c630097ec2df6eb536f43a2bca875b5cf
John Kraal <johnkraal@icloud.com>
2021-12-07 06:28:35 +0100

    2021:7 python

--- 2021/07.py
+++ 2021/07.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
+from functools import cache
 
 from util.aoc import file_to_day
 from util.input import load_data
 
 
 def main(test=False):
-    data = load_data(file_to_day(__file__), test)
-    print(data)
+    positions = list(
+        map(int, load_data(file_to_day(__file__), test)[0].strip().split(","))
+    )
+
+    minfuel = float("inf")
+    minfuel2 = float("inf")
+    for x in range(min(positions), max(positions)):
+        steps = [abs(p - x) for p in positions]
+        distance = sum(steps)
+        distance2 = sum([fuel_cost(x) for x in steps])
+        minfuel = min(distance, minfuel)
+        minfuel2 = min(distance2, minfuel2)
+
+    print("2021:07:1 =", minfuel)
+    print("2021:07:2 =", minfuel2)
+
+
+@cache
+def fuel_cost(distance):
+    return sum(list(range(1, distance + 1)))
 
 
 if __name__ == "__main__":
