cbfa6ed8009134ac5fa63ee1c5c04946486b887f
John Kraal <john@johnkraal.com>
2021-12-10 06:36:18 +0100

    2021:10 python holy shit that's easy
    Small fun fact, I entered the points for the characters wrong, so it
    took me a while to figure out why the test case was off by 52.

--- 2021/10.py
+++ 2021/10.py
@@ -1,14 +1,51 @@
 #!/usr/bin/env python3
 
 import sys
+from statistics import median
 
 from util.aoc import file_to_day
 from util.input import load_data
 
+OPEN = ("(", "[", "{", "<")
+CLOSE = (")", "]", "}", ">")
+POINTS = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+COMPLPOINTS = {
+    "(": 1,
+    "[": 2,
+    "{": 3,
+    "<": 4,
+}
+
 
 def main(test=False):
-    data = load_data(file_to_day(__file__), test)
-    print(data)
+    p1 = 0
+    p2 = []
+
+    for line in (x.strip() for x in load_data(file_to_day(__file__), test)):
+        stack = []
+        for c in line:
+            if c in OPEN:
+                stack.append(c)
+            else:
+                o = stack.pop()
+                s = CLOSE[OPEN.index(o)]
+                if s != c:
+                    p1 += POINTS[c]
+                    break
+        else:
+            p = 0
+            for item in stack[::-1]:
+                p *= 5
+                p += COMPLPOINTS[item]
+            p2.append(p)
+
+    print("2021:10:1 =", p1)
+    print("2021:10:2 =", median(sorted(p2)))
 
 
 if __name__ == "__main__":
