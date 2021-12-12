5ce5d9246dd6a250b7bb98db4f53d8d2708cb3b7
John Kraal <john@johnkraal.com>
2021-12-12 09:21:27 +0100

    2021:12 python

--- 2021/12.py
+++ 2021/12.py
@@ -1,14 +1,49 @@
 #!/usr/bin/env python3
 
 import sys
+from collections import defaultdict
 
 from util.aoc import file_to_day
 from util.input import load_data
 
 
 def main(test=False):
+    nodes = defaultdict(list)
     data = load_data(file_to_day(__file__), test)
-    print(data)
+    for edge in data:
+        parent, child = edge.split("-")
+        nodes[parent].append(child)
+        nodes[child].append(parent)
+
+    print("2021:12:1 =", len(search(nodes, "start", [])))
+    print("2021:12:2 =", len(search(nodes, "start", [], True)))
+
+
+def search(nodes, root, visited, p2=False, little=None):
+    paths = []
+    visited.append(root)
+
+    for node in nodes[root]:
+        if node == "end":
+            paths.append([node])
+        elif node == "start":
+            pass
+        elif node != node.lower():
+            paths = paths + search(nodes, node, visited.copy(), p2, little)
+        else:
+            if node in visited:
+                if p2 and little is None:
+                    paths = paths + search(
+                        nodes, node, visited.copy(), p2, node
+                    )
+            else:
+                paths = paths + search(nodes, node, visited.copy(), p2, little)
+
+    retpaths = []
+    for path in paths:
+        retpaths.append([root] + path)
+
+    return retpaths
 
 
 if __name__ == "__main__":
--- 2021/util/input.py
+++ 2021/util/input.py
@@ -1,6 +1,6 @@
 def load_data(day, test=False):
     with open("./input/%02d%s.txt" % (day, "_test" if test else "")) as rd:
-        return rd.readlines()
+        return [l.strip() for l in rd.readlines()]
 
 
 def load_ints(day, test=False):
